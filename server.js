434c8b66b0f210d61eca54c04a56b79d60843284
phechz <peytonengborg@gmail.com>
2023-11-15 18:04:10 -0700

    add

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use('/api', apiRouter)
 
-  //app.use('/api', apiRouter)
 
   app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'))
